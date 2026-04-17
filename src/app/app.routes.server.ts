a21689ff528f075f69c961a413f0e5d4df8fd138
Saif-Mansour-50 <saif200716@gmail.com>
2026-04-17 17:49:13 +0200

    routes.server-fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'brandDetails/:id/:slug',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'details/:id/:slug',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
