bb5c7a040932eecbd724aeed674688022377c001
Saif-Mansour-50 <saif200716@gmail.com>
2026-04-17 17:22:49 +0200

    prerender-mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,14 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'details/:id/:slug',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'brandDetails/:id/:slug',
-    renderMode: RenderMode.Server,
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
